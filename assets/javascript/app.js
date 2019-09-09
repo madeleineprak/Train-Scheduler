0f6c4fee1a7c1d81eec4793d7bb37a65618cb4f3
Madeleine Prak <madeleineprak@gmail.com>
2019-09-08 21:34:07 -0700

    Set up user input form, schedule table, calculate needed train info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// The web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDMegyB5Wu9ZFv_puKsTK4w9ey4cL7T21I",
     authDomain: "train-scheduler-8778c.firebaseapp.com",
@@ -7,6 +7,40 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "1095289439089",
     appId: "1:1095289439089:web:eda2bc89a85e15dd1decd0"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+// Train information
+var trainInfo = {
+    name: "",
+    destination: "",
+    firstArrival: "",
+    frequency: 0,
+    nextArrival: "",
+    minutesAway: 0
+}
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+
+    // Assign user inputs to train information
+    trainInfo.name = $("#name-input").val().trim();
+    trainInfo.destination = $("#destination-input").val().trim();
+    trainInfo.firstArrival = $("#first-arrival-input").val().trim();
+    trainInfo.frequency = $("#frequency-input").val().trim();
+
+    // Calculate minutes away and next arrival
+    var convertedTime = moment(trainInfo.firstArrival, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    console.log("The current time is " + moment(currentTime).format("hh:mm"));
+    var timeDifference = moment().diff(moment(convertedTime), "minutes");
+    var remainder = timeDifference % trainInfo.frequency;
+
+    // Assign found values to train information
+    trainInfo.minutesAway = trainInfo.frequency - remainder;
+    trainInfo.nextArrival = moment().add(trainInfo.minutesAway, "minutes").format("hh:mm A");
+});
