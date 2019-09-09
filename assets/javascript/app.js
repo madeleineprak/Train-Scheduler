f360286a6b220884578b68fc93a13c71864693a9
Madeleine Prak <madeleineprak@gmail.com>
2019-09-08 19:43:18 -0700

    Set up initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDMegyB5Wu9ZFv_puKsTK4w9ey4cL7T21I",
+    authDomain: "train-scheduler-8778c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8778c.firebaseio.com",
+    projectId: "train-scheduler-8778c",
+    storageBucket: "",
+    messagingSenderId: "1095289439089",
+    appId: "1:1095289439089:web:eda2bc89a85e15dd1decd0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
